0185e9f5b2840da552f5d8faa847bc86fc7dc510
Joanthan Gilyadov <Jonathan.gilyadov@gmail.com>
2021-06-17 16:42:50 +0300

    Update Forecasts.js

--- src/components/Forecasts.js
+++ src/components/Forecasts.js
@@ -1,10 +1,9 @@
-import React, { Fragment, useState, useEffect, useCallback } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { useRouteMatch, Route, Switch, Redirect } from 'react-router-dom';
 import { TextField } from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { getLocationsAutocomplete } from '../ajax';
 import LocationProfile from './LocationProfile';
-import Select from 'react-select';
 import defaultOptions from './defaultOptions.json';
 
 const Forecasts = () => {
@@ -14,11 +13,6 @@ const Forecasts = () => {
 	});
 	const [query, setQuery] = useState('');
 	const [options, setOptions] = useState(defaultOptions);
-	const [isLoading, setLoading] = useState(false);
-	const [err, setErr] = useState({
-		isErr: false,
-		message: '',
-	});
 	let { path } = useRouteMatch();
 
 	useEffect(() => {
