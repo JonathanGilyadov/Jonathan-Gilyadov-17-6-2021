ab3886b95d3b65cd6e1422ee4863a74a86b98b77
Jonathan Gilyadov <joni.gilyadov03@gmail.com>
2021-06-17 17:17:32 +0300

    setting up proxy

--- src/ajax.js
+++ src/ajax.js
@@ -1,15 +1,19 @@
-import { KEY } from "./config";
-import axios from "axios";
+import { KEY } from './config';
+import axios from 'axios';
 
 export const getLocationsAutocomplete = (text) =>
-  axios(`/locations/v1/cities/autocomplete?apikey=${KEY}&q=${text}`);
+	axios(
+		`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${KEY}&q=${text}`
+	);
 
 export const getCurrentConditions = (keyLocation) =>
-  axios(`/currentconditions/v1/${keyLocation}?apikey=${KEY}`);
+	axios(
+		`http://dataservice.accuweather.com/currentconditions/v1/${keyLocation}?apikey=${KEY}`
+	);
 
 export const getFiveDaysForecast = (keyLocation, tempType) =>
-  axios(
-    `/forecasts/v1/daily/5day/${keyLocation}?apikey=${KEY}&metric=${
-      tempType === "CELSIUS"
-    }`
-  );
+	axios(
+		`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${keyLocation}?apikey=${KEY}&metric=${
+			tempType === 'CELSIUS'
+		}`
+	);
